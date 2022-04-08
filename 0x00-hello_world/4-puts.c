b98147ef74828cf54c0b16efbb4c616b3a3cdeb9
Balqees <lasisibalqees12@gmail.com>
2022-04-08 11:08:30 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - prints the phrase "Programming is
+ *like building a multilingual puzzle
+ *Printf function is not used
+ *Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
