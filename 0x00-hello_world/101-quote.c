af6980ac7c3bbfa712df9ed4d59d349ecd60b44b
Balqees <lasisibalqees12@gmail.com>
2022-04-08 12:07:39 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ *main - prints the words "and that piece
+ *of art is useful"
+ *Return: 1 upon completion
+ *
+ */
+
+int main(void)
+{
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+59);
+
+return (1);
+
+}
