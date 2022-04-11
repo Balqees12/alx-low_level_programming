6b83bd393bc469be0bd787068843c7ffb2155788
Balqees <45130490+Balqees12@users.noreply.github.com>
2022-04-11 16:22:44 -0700

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,18 +11,18 @@ int main(void)
 
 char alphabet;
 
-for (alphabet = a; alphabet <= z; alphabet++)
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
 putchar(alphabet);
 
 }
 
-for (alphabet = A; alphabet <= Z; alphabet++)
+for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
 
 {
 putchar(alphabet);
 }
 
-putchar(n);
+putchar('\n');
 return (0);
 }
