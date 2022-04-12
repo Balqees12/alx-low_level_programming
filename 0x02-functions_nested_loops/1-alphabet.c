aa5e6176babdc1a54ba1bbd8d0f4ac7162a23988
Balqees <lasisibalqees12@gmail.com>
2022-04-12 11:17:23 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - prints a-z all lower case with a new line.
+ *
+ * Return: Void.
+*/
+
+void print_alphabet(void)
+{
+int i = 97;
+while (i <= 122)
+{
+_putchar(i);
+i++;
+}
+_putchar(n);
+}
